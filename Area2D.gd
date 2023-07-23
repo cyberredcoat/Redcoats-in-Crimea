3de326f00c4589bf61b4c730f346f4909276c25a
Nerdronaut <90946276+ThatSpaceNerd@users.noreply.github.com>
2023-07-23 11:44:47 +0800

    shortening

--- Area2D.gd
+++ Area2D.gd
@@ -1,6 +1,6 @@
 extends Area2D
 	
 func _physics_process(delta: float) -> void:
-	const move_speed := 100.0
+	const move_speed := 100
 	%PathFollow2D.progress += move_speed * delta
 	
--- scenes/div2/div2.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-@onready var path_follow = get_parent()
-@export var _speed = 0
-
-func _physics_process(delta):
-	path_follow.set_offset(path_follow.get_offset() + _speed * delta)
