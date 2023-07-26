d23264ad2f2b69feb3cf7b4465e84e4ba53979b0
The Elite Redcoat <90946276+TheCodingRedcoat@users.noreply.github.com>
2023-07-26 23:00:15 +0800

    v0.16 26/7 Day Close

--- Area2D.gd
+++
@@ -1,5 +0,0 @@
-extends Area2D
-	
-func _physics_process(delta: float) -> void:
-	const move_speed := 100
-	%PathFollow2D.progress += move_speed * delta
---
+++ BattlesScene.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var fireScene: PackedScene = preload("res://scenes/projectiles/bullet.tscn")
+var enemyFireScene: PackedScene = preload("res://scenes/projectiles/bullet.tscn")
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+func _on_player_player_shoot(pos,dir):
+	var fire = enemyFireScene.instantiate() as Area2D
+	fire.position = pos
+	fire.direction = dir
+	fire.rotation_degrees = rad_to_deg(dir.angle()) + 90
+	$Projectiles.add_child(fire)
+
+func _on_russian_reg_russian_shoot(posi, dir):
+	var enemyFire = enemyFireScene.instantiate() as Area2D
+	enemyFire.position = posi
+	enemyFire.direction = dir
+	enemyFire.rotation_degrees = rad_to_deg(dir.angle()) + 90
+	$ProjectilesEnemy.add_child(enemyFire)
