53ff29fa9fcd1a1f5f8153ec1895e496c00a66b2
The Elite Redcoat <90946276+TheCodingRedcoat@users.noreply.github.com>
2023-07-29 17:09:24 +0800

    v0.183

--- Menu.gd
+++ Menu.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event.is_pressed():
-		get_tree().change_scene_to_file("res://InkermanTitle.tscn")
+		get_tree().change_scene_to_file("res://Intro.tscn")
