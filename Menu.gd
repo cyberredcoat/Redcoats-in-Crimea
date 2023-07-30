97a1211f4848d6953213822f57a388046a6f9d3e
The Elite Redcoat <90946276+TheCodingRedcoat@users.noreply.github.com>
2023-07-30 20:00:32 +0800

    v?

--- Menu.gd
+++ Menu.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event.is_pressed():
 		get_tree().change_scene_to_file("res://Intro.tscn")
